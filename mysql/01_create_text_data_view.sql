7f975a615e6fa1a47d355aac70ca034a9f17d1b0
Ram Luis <ramluismarmol.edu@gmail.com>
2025-09-21 21:44:16 +0800

    Edit semantic feedback flagging

--- mysql/01_create_text_data_view.sql
+++ mysql/01_create_text_data_view.sql
@@ -4,7 +4,7 @@ WITH cte AS (
         hashed_cellphone_number,
         COALESCE(TRIM(text), '') AS text,
         REGEXP_REPLACE(COALESCE(TRIM(text), ''), '[[:space:]]', '') AS no_space_text,
-        REGEXP_REPLACE(COALESCE(TRIM(text), ''), '[A-Z]', '', 1, 0, 'c') AS no_upper_text,
+        REGEXP_REPLACE(COALESCE(TRIM(text), ''), '[A-Z]', '') AS no_upper_text,
         REGEXP_REPLACE(COALESCE(TRIM(text), ''), '[[:digit:]]', '') AS no_digit_text,
         REGEXP_REPLACE(COALESCE(TRIM(text), ''), '[[:punct:]]', '') AS no_punc_text,
         REGEXP_REPLACE(COALESCE(TRIM(text), ''), '[^[:alnum:]]', '') AS alnum_text
@@ -21,9 +21,9 @@ SELECT
     CHAR_LENGTH(text) - CHAR_LENGTH(no_digit_text) AS digit_count,
     CHAR_LENGTH(text) - CHAR_LENGTH(no_punc_text) AS punctuation_count,
     CHAR_LENGTH(text) - CHAR_LENGTH(alnum_text) AS non_alnum_count,
-    REGEXP_LIKE(text, 'http|www|com|bit') AS has_url,
-    REGEXP_LIKE(text, 'free|win|urgent|limited|offer|deal|prize|discount') AS has_spam_keywords,
-    REGEXP_LIKE(text, 'money|cash|payment|credit|bank|account|deposit') AS has_financial_keywords,
-    REGEXP_LIKE(text, 'click|call[[:space:]]*now|subscribe|buy[[:space:]]*now|sign[[:space:]]*up') AS has_action_keywords,
-    REGEXP_LIKE(text, 'verify|account|password|login|personal|security') AS has_info_keywords
+    REGEXP_LIKE(text, 'http|www.|.com|bit.|.site', 'i') AS has_url,
+    REGEXP_LIKE(text, 'free|win|award|reward|claim|urgent|limited|offer|deal|prize|discount', 'i') AS has_spam_keywords,
+    REGEXP_LIKE(text, 'money|cash|payment|credit|bank|account|deposit|php', 'i') AS has_financial_keywords,
+	REGEXP_LIKE(text, 'sale|exclusive|special|bonus|save|get|shop|promo|venue', 'i') AS has_promotional_keywords,
+    REGEXP_LIKE(text, 'verify|account|password|login|personal|security', 'i') AS has_info_keywords
 FROM cte;
