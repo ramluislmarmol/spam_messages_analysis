095eb87ef9e532e42bb4cd1d11bb42dcdb0ea3d3
Ram Luis <ramluismarmol.edu@gmail.com>
2025-09-21 18:48:08 +0800

    Fixed typo in cellphone

--- mysql/00_setup_schema.sql
+++ mysql/00_setup_schema.sql
@@ -2,21 +2,21 @@ CREATE DATABASE IF NOT EXISTS spam_messages_analysis;
 USE spam_messages_analysis;
 
 CREATE TABLE IF NOT EXISTS spam_messages (
-	masked_celphone_number VARCHAR(255),
-    hashed_celphone_number CHAR(36),
+	masked_cellphone_number VARCHAR(255),
+    hashed_cellphone_number CHAR(36),
     date DATETIME(3),
     text TEXT,
     carrier VARCHAR(50)
 );
 
-LOAD DATA LOCAL INFILE '<insert filepath>'
+LOAD DATA LOCAL INFILE 'C:/Users/turnipxd/Documents/ram_files/project_repos/spam_messages_analysis/data/spam_messages.csv'
 INTO TABLE spam_messages
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"' 
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
-(masked_celphone_number,
- hashed_celphone_number,
+(masked_cellphone_number,
+ hashed_cellphone_number,
  date,
  text,
  carrier);
