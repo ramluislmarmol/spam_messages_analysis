6f7517b7f328aba680df176dca029bed5d642a99
Ram Luis <ramluismarmol.edu@gmail.com>
2025-09-21 23:15:43 +0800

    Added primary key (id)

--- mysql/00_setup_schema.sql
+++ mysql/00_setup_schema.sql
@@ -2,6 +2,7 @@ CREATE DATABASE IF NOT EXISTS spam_messages_analysis;
 USE spam_messages_analysis;
 
 CREATE TABLE IF NOT EXISTS spam_messages (
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	masked_cellphone_number VARCHAR(255),
     hashed_cellphone_number CHAR(36),
     date DATETIME(3),
@@ -9,7 +10,7 @@ CREATE TABLE IF NOT EXISTS spam_messages (
     carrier VARCHAR(50)
 );
 
-LOAD DATA LOCAL INFILE '<insert path here>'
+LOAD DATA LOCAL INFILE '<input file here>'
 INTO TABLE spam_messages
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"' 
